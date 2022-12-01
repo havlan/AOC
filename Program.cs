20c7d2e4220b2d5c4bd3d973d35dad8e9b49e3b4
Håvard Langdal <lahavard@microsoft.com>
2022-12-01 13:04:58 +0100

    straight to masta

--- Program.cs
+++ Program.cs
@@ -11,15 +11,12 @@ namespace AOC
     {
         static void Main(string[] args)
         {
-            string directory = Directory.GetCurrentDirectory();
-            string dataDir = "\\input\\";
-            string basePath = string.Format("{0}{1}", directory, dataDir);
-
             var day = args.Length > 0 ? int.Parse(args[0]) : DateTime.Now.Day;
+            var year = DateTime.Now.Year;
 
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            var solver = Solve(day);
+            var solver = Solve(year, day);
             solver.Init();
             stopWatch.Stop();
             Console.WriteLine("Init finished in {0}ms", stopWatch.ElapsedMilliseconds);
@@ -32,33 +29,50 @@ namespace AOC
             stopWatch.Stop();
         }
 
-        private static ISolver Solve(int day)
+        private static ISolver Solve(int year, int day)
         {
             string directory = Directory.GetCurrentDirectory();
-            string dataDir = "\\input\\";
-            string basePath = string.Format("{0}{1}", directory, dataDir);
+#if DEBUG
+            var trimmedDir = directory.Take(directory.IndexOf("bin")); //C: \Users\lahavard\source\repos\AOC\bin\Debug\net6.0\input\2022\1.txt
+            directory = string.Concat(trimmedDir);
+#endif
+            string dataDir = $"\\input\\{year}\\";
+            string basePath = string.Format("{0}{1}{2}.txt", directory, dataDir, day);
 
-            return day switch
+            if (year == 2022)
             {
-                1 => new SonarSweep(basePath + "1.txt"),
-                2 => new Dive(basePath + "2.txt"),
-                3 => new BinaryDiagnostic(basePath + "3.txt"),
-                4 => new GiantSquid(basePath + "4.txt"),
-                5 => new HydrothermalVenture(basePath + "5.txt"),
-                6 => new Lanternfish(basePath + "6.txt"),
-                7 => new TreacheryOfWhales(basePath + "7.txt"),
-                8 => new SevenSegmentSearch(basePath + "8.txt"),
-                9 => new SmokeBasin(basePath + "9.txt"),
-                10 => new SyntaxScoring(basePath + "10.txt"),
-                11 => new DumboOctopus(basePath + "11.txt"),
-                12 => new PassagePathing(basePath + "12.txt"),
-                13 => new TransparentOrigami(basePath + "13.txt"),
-                14 => new ExtendedPoymerization(basePath + "14.txt"),
-                15 => new Chiton(basePath + "15.txt"),
-                16 => new PacketDecoder(basePath + "16.txt"),
-                17 => new TrickShot(basePath + "17.txt"),
-                _ => throw new ArgumentException($"Day {day} not implemented."),
-            };
+                return day switch
+                {
+                    _ => new CalorieCounting(basePath),
+                };
+            }
+
+            if (year == 2021)
+            {
+                return day switch
+                {
+                    1 => new SonarSweep(basePath),
+                    2 => new Dive(basePath),
+                    3 => new BinaryDiagnostic(basePath),
+                    4 => new GiantSquid(basePath),
+                    5 => new HydrothermalVenture(basePath),
+                    6 => new Lanternfish(basePath),
+                    7 => new TreacheryOfWhales(basePath),
+                    8 => new SevenSegmentSearch(basePath),
+                    9 => new SmokeBasin(basePath),
+                    10 => new SyntaxScoring(basePath),
+                    11 => new DumboOctopus(basePath),
+                    12 => new PassagePathing(basePath),
+                    13 => new TransparentOrigami(basePath),
+                    14 => new ExtendedPoymerization(basePath),
+                    15 => new Chiton(basePath),
+                    16 => new PacketDecoder(basePath),
+                    17 => new TrickShot(basePath),
+                    _ => throw new ArgumentException($"Day {day} not implemented for year {year}."),
+                };
+            }
+
+            throw new ArgumentException($"Day {day} not implemented in year {year}.");
         }
     }
 }
---
+++ src/2021/Lanternfish.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AOC
+{
+    public class Lanternfish : ISolver
+    {
+        private readonly string filename;
+        private List<int> data;
+
+        public Lanternfish(string filename)
+        {
+            this.filename = filename;
+        }
+
+        private List<int> ReadData()
+        {
+            var text = File.ReadAllText(this.filename);
+            var data = new List<int>();
+            foreach (var l in text.Split(","))
+            {
+                data.Add(int.Parse(l));
+            }
+
+            return data;
+        }
+
+        public void PartOne()
+        {
+            Console.WriteLine("{0}", getFishies(80));
+        }
+
+        public void PartTwo()
+        {
+            Console.WriteLine("{0}", getFishies(256));
+        }
+
+        private long getFishies(int dayys)
+        {
+            var fishLifeTickToCountMap = new Dictionary<int, long>();
+            var partOneData = data;
+            for (var i = 0; i < 10; i++)
+            {
+                fishLifeTickToCountMap[i] = 0;
+            }
+
+            foreach (var fish in partOneData)
+            {
+                fishLifeTickToCountMap[fish]++;
+            }
+
+            for (var i = 0; i < dayys; i++)
+            {
+                var producedFish = fishLifeTickToCountMap[0];
+
+                // fish is now produced
+                fishLifeTickToCountMap[7] += producedFish;
+                for (var k = 1; k < fishLifeTickToCountMap.Count; k++)
+                { // do whole length?
+                    fishLifeTickToCountMap[k - 1] = fishLifeTickToCountMap[k];
+                }
+
+                fishLifeTickToCountMap[8] = producedFish;
+            }
+
+            return fishLifeTickToCountMap.Sum(s => s.Value);
+        }
+
+        public void Init()
+        {
+            this.data = ReadData();
+        }
+    }
+}
